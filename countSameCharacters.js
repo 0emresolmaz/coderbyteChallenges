b9db0c1fc87c4eb685fc36c7ecf50e15edd14b19
Emre Solmaz <emre.solmaz106@gmail.com>
2021-11-30 02:28:42 +0300

    Update countSameCharacters.js

--- countSameCharacters.js
+++ countSameCharacters.js
@@ -1,24 +1,21 @@
 	// in:  wwwwaaadexxxxxxywww
     // out: 4w3a1d1e6x1y3w  > counting same character 
     
-    function a(str)
-	{
-		let n = str.length;
-    var result='';
-		for (let i = 0; i < n; i++)
-		{
-			// Count occurrences of current character
-			let count = 1;
-			while (i < n - 1 && str[i] == str[i+1])
-			{
-				count++;
-				i++;
-			}
-			
-			result+=count+str[i];
+function a(str) {
+	let n = str.length;
+	var result = '';
+	for (let i = 0; i < n; i++) {
+		// Count occurrences of current character
+		let count = 1;
+		while (i < n - 1 && str[i] == str[i + 1]) {
+			count++;
+			i++;
 		}
-    return result;
+
+		result += count + str[i];
 	}
-	
-  console.log(a("wwwwaaadexxxxxxywww"));
+	return result;
+}
+
+console.log(a("wwwwaaadexxxxxxywww"));
 
