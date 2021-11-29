b829618d7542e706a45e94feb086c9517a4373e7
Emre Solmaz <emre.solmaz106@gmail.com>
2021-11-30 02:27:47 +0300

    Add files via upload

---
+++ countSameCharacters.js
@@ -0,0 +1,24 @@
+	// in:  wwwwaaadexxxxxxywww
+    // out: 4w3a1d1e6x1y3w  > counting same character 
+    
+    function a(str)
+	{
+		let n = str.length;
+    var result='';
+		for (let i = 0; i < n; i++)
+		{
+			// Count occurrences of current character
+			let count = 1;
+			while (i < n - 1 && str[i] == str[i+1])
+			{
+				count++;
+				i++;
+			}
+			
+			result+=count+str[i];
+		}
+    return result;
+	}
+	
+  console.log(a("wwwwaaadexxxxxxywww"));
+
